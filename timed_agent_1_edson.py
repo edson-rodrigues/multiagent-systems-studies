3d7d7255df33414851a6531cb71deb78a30eb209
Edson <edson.rodgon@gmail.com>
2019-10-25 10:27:25 -0300

    timed agent done

--- timed_agent_1_edson.py
+++ timed_agent_1_edson.py
@@ -5,46 +5,38 @@ from pade.acl.aid import AID
 from pade.behaviours.protocols import FipaRequestProtocol
 from pade.behaviours.protocols import TimedBehaviour
 
-# Comportamento do agente Remetente
-
-class RemetenteBehaviour(FipaRequestProtocol):
+class SenderComportamento(FipaRequestProtocol):
     
     def __init__(self, agent,message):
-        super(RemetenteBehaviour, self).__init__(agent=agent,message=message,is_initiator=True)
+        super(SenderComportamento, self).__init__(agent=agent,message=message,is_initiator=True)
 
-    # Este método tem por objetivo exibir a "resposta" da mensagem recebida pelo agente
-    def handle_inform(self, message):
+    def handle_request(self, message):
         display_message(self.agent.aid.localname, 'O agente {} recebeu a mensagem "{}" da conversa {}'.format(self.agent.aid.localname, message.content, message.conversationID))
 
 
-# Comportamento do agente Destinatario
-
-class DestinatarioBehaviour(FipaRequestProtocol):
+class ListenerComportamento(FipaRequestProtocol):
 
     def __init__(self, agent):
-        super(DestinatarioBehaviour, self).__init__(agent=agent,message=None,is_initiator=False)
+        super(ListenerComportamento, self).__init__(agent=agent,message=None,is_initiator=False)
 
-    # Este método tem por objetivo escrever uma resposta para a mensagem recebida   
+    
     def handle_request(self, message):
         
-        super(DestinatarioBehaviour, self).handle_request(message)
+        super(ListenerComportamento, self).handle_request(message)
         
-        # Exibe a mensagem recebida
+        
         display_message(self.agent.aid.localname, 'O agente {} recebeu a mensagem "{}" da conversa {}'.format(self.agent.aid.localname, message.content, message.conversationID))
         
-        # Escreve uma resposta para a mensagem recebida
         reply = message.create_reply()
         reply.set_performative(ACLMessage.INFORM)
-        reply.set_content('Hello!')
+        reply.set_content('Oi, o que deseja?')
         display_message(self.agent.aid.localname, 'Enviando mensagem...')
         self.agent.send(reply)
         display_message(self.agent.aid.localname, 'Mensagem enviada')
 
 
 class ComportTemporal(TimedBehaviour):
-    
-    # Comportamento temporal do remetente
-    
+            
     def __init__(self, agent, time, message):
         super(ComportTemporal, self).__init__(agent, time)
         self.message = message
@@ -55,47 +47,44 @@ class ComportTemporal(TimedBehaviour):
         display_message(self.agent.aid.localname, 'Mensagem enviada')
         
         
-class Remetente(Agent):
+class Sender(Agent):
     
     def __init__(self, aid):
-        # Chamando um construtor da classe 'mãe' = Agente
-        super(Remetente, self).__init__(aid=aid, debug=False)
+        super(Sender, self).__init__(aid=aid, debug=False)
         
         display_message(self.aid.localname, 'Enviando mensagem...')
         
-        # Mensagem a ser enviada
         message = ACLMessage(ACLMessage.REQUEST)
         message.set_protocol(ACLMessage.FIPA_REQUEST_PROTOCOL)
-        message.add_receiver(AID(name='destinatario'))
+        message.add_receiver(AID(name='ouvinte'))
         message.set_content('Ola, eu estou aqui!')
         
-        # Adicionando os comportamentos ao agente
-        self.behaviours.append(RemetenteBehaviour(self, message))
-        # O tempo do comportamento temporal é dado em segundos
+        self.behaviours.append(SenderComportamento(self, message))
+        
         self.behaviours.append(ComportTemporal(self, 5.0, message))
 
 
-class Destinatario(Agent):
+class Listener(Agent):
 
     def __init__(self, aid):
-        # Chamando um construtor da classe 'mãe' = Agente
-        super(Destinatario, self).__init__(aid=aid, debug=False)
         
-        # Adicionando o comportamento do Destinatáro
-        self.behaviours.append(DestinatarioBehaviour(self))
+        super(Listener, self).__init__(aid=aid, debug=False)
+        
+        
+        self.behaviours.append(ListenerComportamento(self))
         
 
 if __name__ == '__main__':
 
     agentes = list()
     
-    remetente_nome = 'remetente@localhost:9000'
-    remetente = Remetente(AID(name=remetente_nome))
-    agentes.append(remetente)
+    sender_nome = 'sender@localhost:9000'
+    sender = Sender(AID(name=sender_nome))
+    agentes.append(sender)
 
-    destinatario_nome = 'destinatario@localhost:9001'
-    destinatario = Destinatario(AID(name=destinatario_nome))
-    agentes.append(destinatario)
+    listener_nome = 'destinatario@localhost:9001'
+    listener = Listener(AID(name=listener_nome))
+    agentes.append(listener)
 
     start_loop(agentes)
 
--- timed_agent_hello_world.py
+++ timed_agent_hello_world.py
@@ -1,36 +1,90 @@
 from pade.misc.utility import display_message, start_loop
 from pade.core.agent import Agent
+from pade.acl.messages import ACLMessage
 from pade.acl.aid import AID
+from pade.behaviours.protocols import FipaRequestProtocol
 from pade.behaviours.protocols import TimedBehaviour
-from sys import argv
+
+class SenderComportamento(FipaRequestProtocol):
+    
+    def __init__(self, agent,message):
+        super(SenderComportamento, self).__init__(agent=agent,message=message,is_initiator=True)
+
+    def handle_request(self, message):
+        display_message(self.agent.aid.localname, 'O agente {} recebeu a mensagem "{}" da conversa {}'.format(self.agent.aid.localname, message.content, message.conversationID))
+
+
+class ListenerComportamento(FipaRequestProtocol):
+
+    def __init__(self, agent):
+        super(ListenerComportamento, self).__init__(agent=agent,message=None,is_initiator=False)
+
+    
+    def handle_request(self, message):
+        
+        super(ListenerComportamento, self).handle_request(message)
+        
+        
+        display_message(self.agent.aid.localname, 'O agente {} recebeu a mensagem "{}" da conversa {}'.format(self.agent.aid.localname, message.content, message.conversationID))
+        
+        reply = message.create_reply()
+        reply.set_performative(ACLMessage.INFORM)
+        reply.set_content('Oi, o que deseja?')
+        display_message(self.agent.aid.localname, 'Enviando mensagem...')
+        self.agent.send(reply)
+        display_message(self.agent.aid.localname, 'Mensagem enviada')
+
 
 class ComportTemporal(TimedBehaviour):
-    def __init__(self, agent, time):
+            
+    def __init__(self, agent, time, message):
         super(ComportTemporal, self).__init__(agent, time)
+        self.message = message
 
     def on_time(self):
         super(ComportTemporal, self).on_time()
-        display_message(self.agent.aid.localname, 'Hello World!')
-
-
-class Agente(Agent):
+        self.agent.send(self.message)
+        display_message(self.agent.aid.localname, 'Mensagem enviada')
+        
+        
+class Sender(Agent):
+    
     def __init__(self, aid):
-        super(Agente, self).__init__(aid=aid, debug=False)
+        super(Sender, self).__init__(aid=aid, debug=False)
+        
+        display_message(self.aid.localname, 'Enviando mensagem...')
+        
+        message = ACLMessage(ACLMessage.REQUEST)
+        message.set_protocol(ACLMessage.FIPA_REQUEST_PROTOCOL)
+        message.add_receiver(AID(name='ouvinte'))
+        message.set_content('Ola, eu estou aqui!')
+        
+        self.behaviours.append(SenderComportamento(self, message))
+        
+        self.behaviours.append(ComportTemporal(self, 5.0, message))
 
-        comp_temp = ComportTemporal(self, 1.0)
 
-        self.behaviours.append(comp_temp)
+class Listener(Agent):
 
+    def __init__(self, aid):
+        
+        super(Listener, self).__init__(aid=aid, debug=False)
+        
+        
+        self.behaviours.append(ListenerComportamento(self))
+        
 
 if __name__ == '__main__':
-    agents_per_process = 2
-    c = 0
-    agents = list()
-    for i in range(agents_per_process):
-        port = int(argv[1]) + c
-        agent_name = 'agent_hello_{}@localhost:{}'.format(port, port)
-        agente_hello = Agente(AID(name=agent_name))
-        agents.append(agente_hello)
-        c += 1000
 
-    start_loop(agents)
+    agentes = list()
+    
+    sender_nome = 'sender@localhost:9000'
+    sender = Sender(AID(name=sender_nome))
+    agentes.append(sender)
+
+    listener_nome = 'destinatario@localhost:9001'
+    listener = Listener(AID(name=listener_nome))
+    agentes.append(listener)
+
+    start_loop(agentes)
+
